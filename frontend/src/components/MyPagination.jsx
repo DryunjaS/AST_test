e485632bb6f0e749aee2f6d655aac07c3eba1133
Sosnin Andrey <KamushekKamushek@gmail.com>
2024-06-18 20:22:46 +0300

    student-test

--- frontend/src/components/MyPagination.jsx
+++ frontend/src/components/MyPagination.jsx
@@ -5,7 +5,12 @@ import { useParams } from "react-router-dom"
 function MyPagination({ userID, value, select, setSelectQ }) {
 	const [show, setShow] = useState(false)
 	const { testID } = useParams()
-
+	const arr = Array.from({ length: value }, (_, index) => index + 1)
+	const handleLeft = () => {
+		if (select > 1) {
+			setSelectQ((n) => n - 1)
+		}
+	}
 	const handleRight = () => {
 		if (select < value) {
 			setSelectQ((n) => n + 1)
@@ -14,16 +19,27 @@ function MyPagination({ userID, value, select, setSelectQ }) {
 			setShow(true)
 		}
 	}
+	const handleItem = (item) => {
+		setSelectQ(item)
+	}
 
 	return (
-		<div>
+		<div className='pagination'>
 			<Module show={show} setShow={setShow} testID={testID} userID={userID} />
-
-			<div className='pagination'>
-				{select} из {value}
-				<div className='pagination__item ' onClick={handleRight}>
-					{select < value ? "Следующий вопрос" : "Заверщить тест"}
+			<div className='pagination__item' onClick={handleLeft}>
+				{"<"}
+			</div>
+			{arr.map((item) => (
+				<div
+					key={item}
+					className={`pagination__item ${item === select ? "active" : ""}`}
+					onClick={() => handleItem(item)}
+				>
+					{item}
 				</div>
+			))}
+			<div className='pagination__item' onClick={handleRight}>
+				{">"}
 			</div>
 		</div>
 	)
